7749cf81a19f8d59676235706afb320ff703a4cf
Slusarok <sliusarchuk_ak21@nuwm.edu.ua>
2021-10-25 13:33:00 +0300

    Create Projekt

---
+++ Projekt_Dota 3/Projekt_Dota 3.cpp
@@ -0,0 +1,15 @@
+﻿// Projekt_Dota 3.cpp: определяет точку входа для консольного приложения.
+//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+
+
+int main()
+{
+    return 0;
+}
+
